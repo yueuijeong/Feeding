df802b979fd0c4055ca7476a64de74e2ad7900a9
D7703_21 <d7703_21@d7703-21ui-imac.local>
2017-11-02 14:14:41 +0900

    Initial Commit

---
+++ free3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  free3
+//
+//  Created by D7703_21 on 2017. 11. 2..
+//  Copyright © 2017년 D7703_21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ free3Tests/free3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  free3Tests.swift
+//  free3Tests
+//
+//  Created by D7703_21 on 2017. 11. 2..
+//  Copyright © 2017년 D7703_21. All rights reserved.
+//
+
+import XCTest
+@testable import free3
+
+class free3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
